00cb54b0430e07555928d62ef83c6fecf99c0836
Jason Ramos <jason@stackideas.com>
2012-04-16 19:18:40 +0800

    $.isUrl should only qualify string that begins with http/https.

--- src/helper.js
+++ src/helper.js
@@ -1,6 +1,6 @@
 $.isUrl = function(s)
 {
-	var regexp = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+	var regexp = /^(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 	return regexp.test(s);
 };
 
